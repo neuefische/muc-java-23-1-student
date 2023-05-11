f55365e2434f37c13f9c252f34763d32f9f8321a
Sergej Grilborzer <sergej@grilborzer.de>
2023-05-11 10:39:08 +0200

    Add 2nd StudentDB test

--- src/test/java/student/StudentDbTest.java
+++ src/test/java/student/StudentDbTest.java
@@ -6,8 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StudentDbTest {
 
-
-
     @Test
     void getAllStudents_shouldReturnOneStudent_whenOneStudentIsInStudentsArray() {
         // GIVEN
@@ -22,4 +20,22 @@ class StudentDbTest {
         // THEN
         assertArrayEquals(expectedStudents, actualStudents);
     }
+
+    @Test
+    void getAllStudents_shouldReturnTwoStudents_whenTwoStudentsAreInStudentsArray() {
+        // GIVEN
+        Student jackTorrance = new Student("1", "Jack Torrance", 37, false);
+        Student wendyTorrance = new Student("2", "Wendy Torrance", 35, false);
+        Student [] expectedStudents = {jackTorrance, wendyTorrance};
+
+        StudentDb studentDb = new StudentDb(expectedStudents);
+
+        // WHEN
+        Student [] actualStudents = studentDb.getAllStudents();
+
+        // THEN
+        assertArrayEquals(expectedStudents, actualStudents);
+    }
+
+
 }
