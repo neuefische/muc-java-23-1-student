8a06dff0308b8d30798214e53fa41815a3f812b3
Sergej Grilborzer <sergej@grilborzer.de>
2023-06-07 15:52:52 +0200

    Update example = Let Spring deal with DI setup in NewsProductionService

--- src/main/java/de/neuefische/mucjava231student/service/NewsProductionService.java
+++ src/main/java/de/neuefische/mucjava231student/service/NewsProductionService.java
@@ -2,13 +2,16 @@ package de.neuefische.mucjava231student.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class NewsProductionService implements NewsPublisher {
 
-    private final List<NewsSubscriber> subscribers = new ArrayList<>();
+    private final List<NewsSubscriber> subscribers;
+
+    public NewsProductionService(List<NewsSubscriber> subscribers) {
+        this.subscribers = subscribers;
+    }
 
     @Override
     public void addSubscriber(NewsSubscriber subscriber) {
